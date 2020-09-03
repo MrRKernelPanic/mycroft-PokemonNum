f2e5a9925a5f7fd84806ad3b03e820d33b62dff8
Mark Routledge <mark@mroutledge.co.uk>
2020-09-03 09:12:33 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -81,9 +81,9 @@ class PokemonNumSkill(MycroftSkill):
         #jprint(response.json())
         nme=response.json()["name"]
         #pokemon_name=self.__jprint(self, nme)
-        pokemon_name=json.dumps(nme, sort_keys=True, indent=4)
+        pokemon_name=str(json.dumps(nme, sort_keys=True, indent=4))
         pokemon_name=pokemon_name.strip('\"')
-        self.speak_dialog('list.pokemon.name', data={'title': pokemon_name})
+        self.speak_dialog('list.pokemon.name', data={"title": pokemon_name})
         #self.speak_dialog(dialog,n})
         # Start showing the remaining time on the faceplate
                     
