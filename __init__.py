d595383657cbf211ca7ee2525f29fc02d0e8a279
Mark Routledge <mark@mroutledge.co.uk>
2020-09-05 13:26:01 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -97,7 +97,7 @@ class PokemonNumSkill(MycroftSkill):
         lcd = character_lcd.Character_LCD_RGB_I2C(i2c, lcd_columns, lcd_rows)
         lcd.color = [100, 0, 0]
         lcd.message = "\nPokemon:" + str(self.pokemon_number)
-        lcd.message = str(self.pokemon_name).strip('\"')   
+        #lcd.message = str(self.pokemon_name).strip('\"')   
   
     
     def stop(self):
